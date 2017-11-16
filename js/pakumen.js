e6b211222c5c27289f0aa9055c521f56d81fa45d
eiji-ito <eiji.ito@gree.co.jp>
2017-11-16 19:23:34 +0900

    pakumen update and add renda game

--- js/pakumen.js
+++ js/pakumen.js
@@ -9,27 +9,40 @@ const MAX_POSITION=2;
 var character = function(position){
     this.position = position;
 };
-var pakumen = new character(1);
+var pakumen = new character(2);
+var isAteming = false;
 
 $(window).keydown(function(e){
+    if (isAteming) {
+        return;
+    }
     switch(e.keyCode) {
+        case KEY_CODE_RIGHT:
+            isAteming = true;
+            $('#pakumen').animate({marginLeft: '+=50px'}, 50).queue(function() {
+                $(this).animate({marginLeft: '-=50px'}, 50).queue(function() {
+                    isAteming = false;
+                    $(this).dequeue();
+                });
+                $(this).dequeue();
+            });
+            break;
         case KEY_CODE_UP:
             if(pakumen.position < MAX_POSITION) {
                 pakumen.position += 1;
-                console.log( pakumen.position );
                 $('#pakumen').animate({
-                    marginTop: '-=150px'
-                }, 200);
+                    marginTop: '-=130px',
+                    queue: false
+                }, 100);
             }
             break;
         case KEY_CODE_DOWN:
             if(pakumen.position > MIN_POSITION) {
                 pakumen.position -= 1;
-                console.log(pakumen.position);
                 $('#pakumen').animate({
-                    marginTop: '+=150px',
+                    marginTop: '+=130px',
                     queue: false
-                }, 200);
+                }, 100);
             }
             break;
         default:
