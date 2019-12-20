396463008367f27f021dd26ceb32bdb578d8c34c
eiji-ito <eiji.ito@gree.net>
2019-12-20 13:27:28 +0900

    fix error

--- js/summon.js
+++ js/summon.js
@@ -2,5 +2,5 @@ jQuery(function(){
     var dpr = window.devicePixelRatio || 1;
     $('#unityContainer').css('width', 1024 * dpr);
     $('#unityContainer').css('height', 768 * dpr);
-    $('#unityContainer').scale(1 / dpr, 1 / dpr);
+    // $('#unityContainer').scale(1 / dpr, 1 / dpr);
 });
