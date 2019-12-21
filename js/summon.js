b2efa9296a399dd4df8dcbc00c37356c3270b6d7
eiji-ito <eiji.ito@gree.net>
2019-12-21 20:47:50 +0900

    test

---
+++ js/summon.js
@@ -0,0 +1,6 @@
+jQuery(function(){
+    var dpr = window.devicePixelRatio || 1;
+    $('#unityContainer').css('width', 1024 * dpr);
+    $('#unityContainer').css('height', 768 * dpr);
+    // $('#unityContainer').scale(1 / dpr, 1 / dpr);
+});
